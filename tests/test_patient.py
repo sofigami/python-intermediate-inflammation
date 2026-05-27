83cb84698614d9f3dce029190f9ab6c4a97b6a85
Sofia Kotti <sofiakotti@gmail.com>
2026-05-27 10:52:03 +0200

    Add Patient class and adapt test_patient

--- tests/test_patient.py
+++ tests/test_patient.py
@@ -5,6 +5,10 @@ from inflammation.models import Patient
 def test_create_patient():
 
     name = 'Alice'
-    p = Patient(name=name)
+    weight = 50
+    height = 1.8
+    p = Patient(name=name, weight = weight, height=height)
 
     assert p.name == name
+    assert p.weight == weight
+    assert p.height == height
