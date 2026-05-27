6bd2ceedf9d15be9a466646d81b61571c1fd1034
Sofia Kotti <sofiakotti@gmail.com>
2026-05-27 10:24:22 +0200

    Move load data to a separate function

--- inflammation/analysis.py
+++ inflammation/analysis.py
@@ -8,6 +8,12 @@ import argparse
 
 from inflammation import models, views
 
+def load_inflammation_data(data_dir):
+    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
+    if len(data_file_paths) == 0:
+        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
+    data = map(models.load_csv, data_file_paths)
+    return list(data)
 
 def analyse_data(data_dir):
     """Calculates the standard deviation by day between datasets.
@@ -15,23 +21,27 @@ def analyse_data(data_dir):
     Gets all the inflammation data from CSV files within a directory,
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
-    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
-    data = map(models.load_csv, data_file_paths)
-
-
+    
+    data = load_inflammation_data(data_dir)
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
 
     daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+    
+    graph_data = {
+        'standard deviation by day': daily_standard_deviation,
+    }
+    views.visualize(graph_data)
+    return(daily_standard_deviation)
 
+    """
     print(daily_standard_deviation)
     return(daily_standard_deviation)
     graph_data = {
         'standard deviation by day': daily_standard_deviation,
     }
     views.visualize(graph_data)
+    """
 
 if __name__ == '__main__':
     # Initialize the parser
