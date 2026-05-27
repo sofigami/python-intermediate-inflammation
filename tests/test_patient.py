5b7164eea39b015de9b206c476b136940ac52769
Sofia Kotti <sofiakotti@gmail.com>
2026-05-27 10:58:10 +0200

    Added test for compute_bmi

--- tests/test_patient.py
+++ tests/test_patient.py
@@ -2,6 +2,8 @@
 
 from inflammation.models import Patient
 
+import numpy.testing as npt
+
 def test_create_patient():
 
     name = 'Alice'
@@ -12,3 +14,19 @@ def test_create_patient():
     assert p.name == name
     assert p.weight == weight
     assert p.height == height
+
+def test_compute_bmi():
+    name = 'maria'
+    weight = 60
+    height = 1.6
+    p = Patient(name=name,  height=height, weight = weight)
+
+    result = p.get_body_mass_index()
+    expected_result = 23.4375
+    npt.assert_almost_equal(result, expected_result)
+
+
+
+
+
+
