7a33a597edcaabdc35625325db36ee5103af3c94
Sofia Kotti <sofiakotti@gmail.com>
2026-05-27 13:45:15 +0200

    Purity exercise

--- inflammation/analysis.py
+++ inflammation/analysis.py
@@ -30,6 +30,14 @@ class JSONDataSource:
         data = map(models.load_json, data_file_paths)
         return list(data)
 
+def compute_standard_deviation_by_day(data):
+    means_by_day = map(models.daily_mean, data)
+    means_by_day_matrix = np.stack(list(means_by_day))
+
+    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+    return daily_standard_deviation    
+
+
 def analyse_data(data_source):
     """Calculates the standard deviation by day between datasets.
 
@@ -38,10 +46,8 @@ def analyse_data(data_source):
     then plots the graphs of standard deviation of these means."""
     
     data = data_source.load_inflammation_data()
-    means_by_day = map(models.daily_mean, data)
-    means_by_day_matrix = np.stack(list(means_by_day))
-
-    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+    
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
     
     graph_data = {
         'standard deviation by day': daily_standard_deviation,
